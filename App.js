22ee515f493a7a202449978abda0ac69dff2d6bc
Christina Roufosse <roufosse.christina@gmail.com>
2019-08-02 21:24:20 -0700

    First Attempt

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View,  ImageBackground } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, ImageBackground, Button } from "react-native";
 
-export default function App() {
-  return (
-    <ImageBackground source={require('./app/img/waterbg.jpg')} style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </ImageBackground>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+    this.buttonClickListener = this.buttonClickListener.bind(this);
+  }
+
+  buttonClickListener = () => {
+    this.setState({
+      counter: counter++
+    });
+  };
+
+  render() {
+    return (
+      <ImageBackground
+        source={require("./app/img/waterbg.jpg")}
+        style={styles.container}
+      >
+        <View style={styles.inner}>
+          <Text style={styles.headerText}>
+            How many glasses of water did you have today?
+          </Text>
+          <Text>{this.state.counter}</Text>
+          <View style={styles.button}>
+            <Button
+              style={styles.innerbutton}
+              onPress={this.buttonClickListener}
+              title="Add"
+              color="#00bfff"
+            />
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
-x
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  inner: {
+    width: "75%",
+    height: "75%",
+    backgroundColor: "rgba(255, 255, 255, .7)"
+  },
+  headerText: {
+    fontSize: 20,
+    color: "#2F4F4F",
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+    // marginBottom: 50
+  },
+  innerbutton: {
+    margin: 20
+  }
 });
